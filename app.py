8d80ceec7c958ac8c46caf95a8b10dcd56ab038f
bpschmitt <bradleypschmitt@gmail.com>
2019-08-15 18:16:37 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello")
+from faker import Faker
+import json
+
+fake = Faker()
+
+def getNum(min,max):
+    return fake.pyint(min_value=min,max_value=max)
+
+
+print(json.dumps({"somethingElse": getNum(0,50), "orderCount" : getNum(100,400), }))
